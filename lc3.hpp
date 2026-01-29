cd2915b99ed721384aff43dc70507380c828b5c4
Abdelaziz Masnaoui <abmasnao@e2r7p15.1337.ma>
2026-01-29 15:20:06 +0100

    hpp

---
+++ lc3.hpp
@@ -0,0 +1,62 @@
+#ifndef LC3_HPP
+#define LC3_HPP
+
+#include <cstdint>
+
+typedef uint16_t i16;
+
+#define MEMORY_MAX (1 << 16)
+
+enum
+{
+    R_R0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC,
+    R_COND,
+    R_COUNT
+};
+
+enum
+{
+    FL_POS = 1 << 0, // P
+    FL_ZRO = 1 << 1, // Z
+    FL_NEG = 1 << 2  // N
+};
+
+enum
+{
+    OP_BR, // branch
+    OP_ADD, // add
+    OP_LD, // load
+    OP_ST, // store
+    OP_JSR, // jump register
+    OP_AND, // bitwise and
+    OP_LDR, // load register
+    OP_STR, // store register
+    OP_RTI, // unused
+    OP_NOT, // bitwise not
+    OP_LDI, // load indirect
+    OP_STI, // store indirect
+    OP_JMP, // jump
+    OP_RES, // reserved (unused)
+    OP_LEA, // load effective address
+    OP_TRAP // execute trap
+};
+
+class lc3
+{
+    public:
+        i16 reg[R_COUNT];
+        i16 memory[MEMORY_MAX];
+};
+
+#endif
+
+// 1111   1111   1111   1111
+// opcode param1 param2 param3
